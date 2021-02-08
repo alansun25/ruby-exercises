a652cf3ca3756d1d13ccb03a4965012ffc914e66
Alan Sun <mralansun@gmail.com>
2021-02-08 15:37:33 -0500

    Completed

--- basic-exercises/caesar-cipher.rb
+++ basic-exercises/caesar-cipher.rb
@@ -1,43 +1,24 @@
 require 'pry'
 
 def caesar_cipher(str, shift)
-    # map each character's unicode value to the shifted value, then convert them back to characters.
     shifted = str.bytes.map do |code|
-        uppercase = code.between?(65, 90)
-        lowercase = code.between?(97, 122)
-        upperOrLower = code < 97 ? 65 : 97
-
-        if uppercase || lowercase
-            if (uppercase && code + shift > 90) || (lowercase && code + shift > 122)
-                code = code - wrap(shift) # wrap backward
-            elsif (uppercase && code + shift < 65) || (lowercase && code + shift < 97)
-                code = code + wrap(shift) # wrap forward
-            else 
-                code = code + shift # when no wrap back is needed
-            end
+        if code.between?(65, 90) || code.between?(97, 122)
+            upperOrLower = code < 97 ? 65 : 97
+            code = ((code + shift - upperOrLower) % 26 + 26) % 26 + upperOrLower
         end
-
         code = code.chr
     end
 
-    puts shifted.join
-end
-
-def wrap(shift)
-    shift = shift.abs
-    n = 26
-    while n < shift
-        n += 26
-    end
-    shift = n - shift
+    shifted.join
 end
 
 ### Tests ###
-caesar_cipher("What a string!", 5) # "Bmfy f xywnsl!"
-caesar_cipher("Hello, World!", 5) # "Mjqqt, Btwqi!"
-caesar_cipher("Mjqqt, Btwqi!", -5) # "Hello, World!"
-caesar_cipher("Z", 1) # "A"
-caesar_cipher("A", -1) # "Z"
-caesar_cipher("Hello, World!", 75) # "Ebiil, Tloia!"
-caesar_cipher("Ebiil, Tloia!", -75) # "Hello, World!"
-caesar_cipher("My name is Alan.", 145) # "Xj ylxp td Lwly."
+p caesar_cipher("Z", 1) # "A"
+p caesar_cipher("A", -1) # "Z"
+p caesar_cipher("What a string!", 5) # "Bmfy f xywnsl!"
+p caesar_cipher("Hello, World!", 5) # "Mjqqt, Btwqi!"
+p caesar_cipher("Mjqqt, Btwqi!", -5) # "Hello, World!"
+p caesar_cipher("Hello, World!", 75) # "Ebiil, Tloia!"
+p caesar_cipher("Ebiil, Tloia!", -75) # "Hello, World!"
+p caesar_cipher("My name is Alan.", 145) # "Bn cpbt xh Papc."
+p caesar_cipher("Bn cpbt xh Papc.", -145) # "My name is Alan."

96d46f02f5bf399d01503a87e9e4125ea5c24085
Alan Sun <mralansun@gmail.com>
2021-02-08 15:24:05 -0500

    Doesn't work on all test cases yet.

--- basic-exercises/caesar-cipher.rb
+++ basic-exercises/caesar-cipher.rb
@@ -1,16 +1,43 @@
 require 'pry'
 
 def caesar_cipher(str, shift)
-    # TODO: Implement
-    
-    # split = str.split(//)
-    # split.each do |char|
-    #     char = char.to_i(base=26)
-    #     char += shift
-    #     char = char.to_s(base=26)
-    # end
-    # split = split.join
-    # puts split
+    # map each character's unicode value to the shifted value, then convert them back to characters.
+    shifted = str.bytes.map do |code|
+        uppercase = code.between?(65, 90)
+        lowercase = code.between?(97, 122)
+        upperOrLower = code < 97 ? 65 : 97
+
+        if uppercase || lowercase
+            if (uppercase && code + shift > 90) || (lowercase && code + shift > 122)
+                code = code - wrap(shift) # wrap backward
+            elsif (uppercase && code + shift < 65) || (lowercase && code + shift < 97)
+                code = code + wrap(shift) # wrap forward
+            else 
+                code = code + shift # when no wrap back is needed
+            end
+        end
+
+        code = code.chr
+    end
+
+    puts shifted.join
 end
 
-caesar_cipher("What a string!", 5)
+def wrap(shift)
+    shift = shift.abs
+    n = 26
+    while n < shift
+        n += 26
+    end
+    shift = n - shift
+end
+
+### Tests ###
+caesar_cipher("What a string!", 5) # "Bmfy f xywnsl!"
+caesar_cipher("Hello, World!", 5) # "Mjqqt, Btwqi!"
+caesar_cipher("Mjqqt, Btwqi!", -5) # "Hello, World!"
+caesar_cipher("Z", 1) # "A"
+caesar_cipher("A", -1) # "Z"
+caesar_cipher("Hello, World!", 75) # "Ebiil, Tloia!"
+caesar_cipher("Ebiil, Tloia!", -75) # "Hello, World!"
+caesar_cipher("My name is Alan.", 145) # "Xj ylxp td Lwly."

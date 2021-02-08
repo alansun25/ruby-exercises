c76a9185677909eb2d02fa523b87a1cc85e9f672
Alan Sun <mralansun@gmail.com>
2021-02-08 02:19:30 -0500

    Started Caesar Cipher

---
+++ basic-exercises/caesar-cipher.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+def caesar_cipher(str, shift)
+    # TODO: Implement
+    
+    # split = str.split(//)
+    # split.each do |char|
+    #     char = char.to_i(base=26)
+    #     char += shift
+    #     char = char.to_s(base=26)
+    # end
+    # split = split.join
+    # puts split
+end
+
+caesar_cipher("What a string!", 5)
